378667efa73da585156af78d1f7f7e582ce4ef43
JaredAdamsCode <jadams29@rams.colostate.edu>
2020-08-27 10:49:06 -0600

    attempt to fix webpacker

---
+++ public/precache-manifest.a045e0ebeb0fbb46cd19991b265b8940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85269716635a38a2f40805633a39d723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e5c32d92437ede48872",
+    "url": "/static/css/main.f4818536.chunk.css"
+  },
+  {
+    "revision": "9dfa46b3da4c1286620b",
+    "url": "/static/js/2.dfb30219.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dfb30219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5c32d92437ede48872",
+    "url": "/static/js/main.a54aae21.chunk.js"
+  },
+  {
+    "revision": "2fee0c8e8525a4664328",
+    "url": "/static/js/runtime-main.2793fa05.js"
+  }
+]);
