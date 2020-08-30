1488a9ceebaa7e658c133226973cb8bb4f966306
JaredAdamsCode <jadams29@rams.colostate.edu>
2020-08-29 22:48:16 -0600

    fix pet mailer

--- app/mailers/pet_mailer.rb
+++ app/mailers/pet_mailer.rb
@@ -4,14 +4,14 @@ class PetMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @specialtext = 'Hello #{user.username}, Welcome to BradsPets!'
-    mail(to: '#{user.email}', subject: 'Welcome to BradsPets')
+    mail(to: @user.email, subject: 'Welcome to BradsPets')
   end
 
   def message_user(message)
     @message = message
     @user = User.find_by(id: message.send_to_user_id)
     @specialtext = @message.message_text
-    mail(to: '#{user.email}', subject: 'You Found My Pet!')
+    mail(to: @user.email, subject: 'You Found My Pet!')
   end
 
 end
