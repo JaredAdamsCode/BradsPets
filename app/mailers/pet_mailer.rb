0a5968cdc9837289ea9e41ae30b08bb71b510418
JaredAdamsCode <jadams29@rams.colostate.edu>
2020-08-29 22:41:08 -0600

    edits to Pet Mailer

--- app/mailers/pet_mailer.rb
+++ app/mailers/pet_mailer.rb
@@ -1,16 +1,17 @@
-class PetMailer < ApplicationMailer::Base
+class PetMailer < ApplicationMailer
   default from: 'bradspets@gmail.com'
   
   def welcome_email(user)
     @user = user
-    @special_text = 'Hello #{user.username}, Welcome to BradsPets!'
+    @specialtext = 'Hello #{user.username}, Welcome to BradsPets!'
     mail(to: '#{user.email}', subject: 'Welcome to BradsPets')
   end
 
   def message_user(message)
+    @message = message
     @user = User.find_by(id: message.send_to_user_id)
-    @special_text = message.message_text
+    @specialtext = @message.message_text
     mail(to: '#{user.email}', subject: 'You Found My Pet!')
   end
-  
+
 end
